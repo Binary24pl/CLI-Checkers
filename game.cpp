85e34a44d5bb8b6af517ab8a4c9def2d9e5abbe8
Binary24pl <jan@Thinkpad.tvk.torun.pl>
2025-07-13 15:39:52 +0200

    no message

--- game.cpp
+++ game.cpp
@@ -4,12 +4,6 @@ int main()
 {
     std::locale::global(std::locale("")); //making special characters work
 
-    ITF::itf_tile* test = nullptr;
-    test = new itf_tile(1,1);
-    test->init();
-
-    delete test;
-
     std::locale::global(std::locale::classic()); //clears the special characters interpreter
     return 0;
 }
