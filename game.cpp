9e54533eebb7b1014dd880f04562832f2ec67450
Binary24pl <scpbinary@gmail.com>
2025-07-13 15:41:07 +0200

    test of my global git variables

--- game.cpp
+++ game.cpp
@@ -4,6 +4,8 @@ int main()
 {
     std::locale::global(std::locale("")); //making special characters work
 
+    std::cout << std::endl;
+
     std::locale::global(std::locale::classic()); //clears the special characters interpreter
     return 0;
 }
