933276ee2835968ba592b03fd8a8e7ef88ea331e
Binary24pl <scpbinary@gmail.com>
2025-07-14 10:20:46 +0200

    Removed previous test for journal clarity, for future tests

--- game.cpp
+++ game.cpp
@@ -12,26 +12,7 @@ int main()
 
     test->paint_fill(ITF_C_WHITE, ITF_HIGH_INTEN);
 
-    test->paint_pixel(ITF_C_RED, ITF_LOW_INTEN);
-    test->draw_pencil.on_hght = 7;
-    test->draw_pencil.on_wdth = 7;
-    test->paint_pixel(ITF_C_GREEN, ITF_LOW_INTEN);
-    test->create_new_layer(ITF_MOVE_PEN);
-
-    test->paint_line(ITF_C_BLUE, ITF_HIGH_INTEN, ITF_D_UP, 7);
-    test->paint_line(ITF_C_RED, ITF_HIGH_INTEN, ITF_D_LEFT, 7);
-
-    test->draw_pencil.on_layer = 0;
-    test->draw_pencil.on_wdth = 0;
-    test->paint_line(ITF_C_RED, ITF_LOW_INTEN, ITF_D_RIGHT, 8);
-
-    test->draw_pencil.on_hght = 0;
-    test->draw_pencil.on_wdth = 2;
-    test->paint_line(ITF_C_BLACK, ITF_HIGH_INTEN, ITF_D_DOWN, 4);
-
     test->create_build();
-    test->create_buffer();
-    test->create_buffer();
     test->create_buffer();
 
     for(int i = 0; i < test->tile_buffer.size(); i++) {
