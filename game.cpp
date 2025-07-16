5793611612059aca268d363734da7d0745174a90
Binary24pl <scpbinary@gmail.com>
2025-07-16 09:15:30 +0200

    Added display of the preload tiles

--- game.cpp
+++ game.cpp
@@ -9,6 +9,16 @@ int main()
     std::vector<ITF::itf_tile*> preload_content;
     ITF::itf_preload_content(preload_content);
 
+    std::cout << std::endl;
+    for(int show = 0; show < ITF_COUNT_IDX; show++)
+    {
+        itf_tile* working_tile = preload_content[show];
+        for(int buf_show = 0; buf_show < working_tile->tile_buffer.size(); buf_show++) {
+            std::cout << working_tile->tile_buffer[buf_show] << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
     ITF::itf_clean_content(preload_content);
     std::locale::global(std::locale::classic()); //clears the special characters interpreter
     return 0;
