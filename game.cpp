3f7e55484c4284bb2f570632b12cbea41616546d
Binary24pl <scpbinary@gmail.com>
2025-07-19 08:33:09 +0200

    translator works well :3

--- game.cpp
+++ game.cpp
@@ -5,6 +5,7 @@ int main()
     std::locale::global(std::locale("")); //making special characters work
 
     std::cout << (char)((int)'A' + 1) << " " << (int)'a' << " " << (int)'A' << std::endl;
+    std::cout << common_translate_value<int, std::string>(123).length() << " " << common_translate_value<std::string, int>("222") << std::endl;
 
     ITF::itf_board* global_board;
     global_board = new itf_board(10,10);
