559bc3b914a5e4ef6aa8e6bda8a76e3cc9289c4b
Binary24pl <scpbinary@gmail.com>
2025-07-20 16:31:42 +0200

    Made checks

--- objects/itf_query_elm.h
+++ objects/itf_query_elm.h
@@ -42,23 +42,39 @@ bool itf_query_elm<input_type>::validate()
 template<typename input_type>
 bool itf_query_elm<input_type>::check_range_from_to()
 {
-    //soon
+    for(int check = 0; check < this->range_of_input->args_amn; check += 2) {
+        if(this->input_val >= this->range_of_input->containter[check] && this->input_val <= this->range_of_input->containter[check + 1]) return true;
+    }
+
+    return false;
 }
 
 template<typename input_type>
 bool itf_query_elm<input_type>::check_range_beyond()
 {
-    //soon
+    for(int check = 0; check < this->range_of_input->args_amn; check += 2) {
+        if(this->input_val >= this->range_of_input->containter[check] && this->input_val <= this->range_of_input->containter[check + 1]) return false;
+    }
+
+    return true;
 }
 
 template<typename input_type>
 bool itf_query_elm<input_type>::check_range_is_in()
 {
-    //soon
+    for(int check = 0; check < this->range_of_input->args_amn; check++) {
+        if(this->input_val == this->range_of_input->containter[check]) return true;
+    }
+
+    return false;
 }
 
 template<typename input_type>
 bool itf_query_elm<input_type>::check_range_is_not_in()
 {
-    //soon
+    for(int check = 0; check < this->range_of_input->args_amn; check++) {
+        if(this->input_val == this->range_of_input->containter[check]) return false;
+    }
+
+    return true;
 }
