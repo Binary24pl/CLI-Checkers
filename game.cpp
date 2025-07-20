1c0404a19cbd74330ccc10348cd717579cc835c6
Binary24pl <scpbinary@gmail.com>
2025-07-20 15:24:03 +0200

    Test went well

--- game.cpp
+++ game.cpp
@@ -4,6 +4,11 @@ int main()
 {
     std::locale::global(std::locale("")); //making special characters work
 
+    ITF::itf_query_elm<std::string>* test;
+    test = new ITF::itf_query_elm<std::string>;
+
+    delete test;
+
     std::locale::global(std::locale::classic()); //clears the special characters interpreter
     return 0;
 }
---
+++ objects/itf_query_elm.h
@@ -0,0 +1,4 @@
+template<typename input_type>
+void itf_query_elm<input_type>::test(input_type val) {
+    std::cout << val << std::endl;
+}
