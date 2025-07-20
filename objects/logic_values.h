58301c2181c61d3a2178981290b966242e6aa905
Binary24pl <scpbinary@gmail.com>
2025-07-20 10:42:28 +0200

    Doing da nodes

--- objects/logic_values.h
+++ objects/logic_values.h
@@ -1,13 +1,31 @@
 #ifndef LOGIC
 #define LOGIC
 
+enum logic_whatami_node {
+    ITF_NODE_NONE
+};
+
+struct logic_passage_node {
+    bool* scores;
+    int* indexies;
+    int count;
+};
+
 class logic_node {
 public:
-    logic_node() {};
+    logic_node(const logic_whatami_node& identity) {
+        this->whatami = identity;
+    };
 
     ~logic_node() {};
-private:
-//soon
+
+    logic_whatami_node whatami;
+protected:
+    bool* prev_scores;
+    int* prev_idx;
+    int prev_count;
+
+    bool output;
 };
 
 #endif
