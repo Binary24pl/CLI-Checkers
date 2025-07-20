c2d33d728b9b1d2fd7af397bc67d66893e88a306
Binary24pl <scpbinary@gmail.com>
2025-07-20 16:10:29 +0200

    succesful stuff

--- game.cpp
+++ game.cpp
@@ -4,10 +4,27 @@ int main()
 {
     std::locale::global(std::locale("")); //making special characters work
 
-    ITF::itf_query_elm<std::string>* test;
-    test = new ITF::itf_query_elm<std::string>;
+    ITF::itf_query_elm<int>* test;
+    test = new ITF::itf_query_elm<int>;
+
+    const int length = 3;
+    ITF::itf_input_range<int> passer;
+    passer.containter = new int[length];
+    passer.args_amn = length;
+    passer.what_range = ITF_RANGE_IS_IN;
+
+    passer.containter[0] = 1;
+    passer.containter[1] = 3;
+    passer.containter[2] = 9;
+
+    test->set_range(passer);
+    test->set_range(passer);
+    test->set_range(passer);
+    test->set_range(passer);
+    test->set_range(passer);
 
     delete test;
+    delete[] passer.containter;
 
     std::locale::global(std::locale::classic()); //clears the special characters interpreter
     return 0;
