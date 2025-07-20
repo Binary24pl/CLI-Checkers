87eebf320a3c4f1d698ab751f7c062fc64fb7a48
Binary24pl <scpbinary@gmail.com>
2025-07-20 11:54:56 +0200

    Removed scrapped idea

--- game.cpp
+++ game.cpp
@@ -4,16 +4,6 @@ int main()
 {
     std::locale::global(std::locale("")); //making special characters work
 
-    LOGIC::logic_node* test;
-    test = new LOGIC::logic_node(LOGIC_NODE_NONE);
-
-    test->connected_from(1,1);
-    test->connected_from(1,1);
-    test->connected_from(1,1);
-    test->connected_from(1,1);
-    test->connected_from(1,1);
-
-    delete test;
 
     std::locale::global(std::locale::classic()); //clears the special characters interpreter
     return 0;
--- objects/commons.h
+++ objects/commons.h
@@ -5,11 +5,7 @@
 
 template<typename FROM, typename TO> TO common_translate_value(FROM to_translate);
 
-#include "logic_values.h"
 #include "itf_values.h"
-
-#include "logic_funcs.h"
-#include "logic_node.h"
 
 #include "itf_funcs.h"
 #include "itf_tile.h"
