a76e80dfa40d0aff91a2928813d352dcbbea3168
Binary24pl <scpbinary@gmail.com>
2025-07-21 10:38:17 +0200

    After this short experiment, I will rewrite question control system after work

--- objects/commons.h
+++ objects/commons.h
@@ -4,6 +4,8 @@
 #include <sstream>
 
 template<typename FROM, typename TO> TO common_translate_value(FROM to_translate);
+void common_create_void_arr(void **&arr, const int& size);
+template<typename CAST>CAST common_get_elm_void_arr(void **&arr, const int& idx);
 
 #include "itf_values.h"
 
@@ -18,6 +20,18 @@ template<typename FROM, typename TO> TO common_translate_value(FROM to_translate
 
     translator << to_translate;
     translator >> to_return;
+
+    return to_return;
+}
+
+void common_create_void_arr(void **&arr, const int& size) {
+    arr = (void **)malloc(sizeof(void*) * size);
+}
+
+template<typename CAST>CAST common_get_elm_void_arr(void **&arr, const int& idx) {
+    void* ptr = arr[idx];
+    CAST* casted = (CAST*)ptr;
+    CAST to_return = *casted;
 
     return to_return;
 }
