963c91de5efaca88d08f2019f8644062a1681411
Binary24pl <scpbinary@gmail.com>
2025-07-21 20:41:03 +0200

    Time for codding

--- game.cpp
+++ game.cpp
@@ -3,27 +3,7 @@
 int main()
 {
     std::locale::global(std::locale("")); //making special characters work
-
-    const int size = 2;
-    void **arr;
-
-    common_create_void_arr(arr, size);
-
-    int *x = new int(2);
-    char *y = new char('U');
-
-    arr[0] = (void*)x;
-    arr[1] = (void*)y;
-
-    std::cout << common_get_elm_void_arr<int>(arr, 0) << std::endl;
-    std::cout << common_get_elm_void_arr<char>(arr, 1) << std::endl;
-
-    delete arr[0];
-    delete arr[1];
     
-
-    free(arr);
-
     std::locale::global(std::locale::classic()); //clears the special characters interpreter
     return 0;
 }
--- objects/commons.h
+++ objects/commons.h
@@ -4,8 +4,6 @@
 #include <sstream>
 
 template<typename FROM, typename TO> TO common_translate_value(FROM to_translate);
-void common_create_void_arr(void **&arr, const int& size);
-template<typename CAST>CAST common_get_elm_void_arr(void **&arr, const int& idx);
 
 #include "itf_values.h"
 
@@ -20,18 +18,6 @@ template<typename FROM, typename TO> TO common_translate_value(FROM to_translate
 
     translator << to_translate;
     translator >> to_return;
-
-    return to_return;
-}
-
-void common_create_void_arr(void **&arr, const int& size) {
-    arr = (void **)malloc(sizeof(void*) * size);
-}
-
-template<typename CAST>CAST common_get_elm_void_arr(void **&arr, const int& idx) {
-    void* ptr = arr[idx];
-    CAST* casted = (CAST*)ptr;
-    CAST to_return = *casted;
 
     return to_return;
 }
