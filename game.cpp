b377c180ad30dc8e1a29112119931f5d6104baa9
Binary24pl <scpbinary@gmail.com>
2025-07-22 13:53:59 +0200

    figured to make getline without getline, cuz getline eats last element

--- game.cpp
+++ game.cpp
@@ -4,6 +4,20 @@ int main()
 {
     std::locale::global(std::locale("")); //making special characters work
 
+    std::vector<std::string> question;
+    std::string word;
+
+    while (std::cin >> word)
+    {
+        question.push_back(word);
+        if(std::cin.peek() == '\n') break;
+    }
+
+    for(int i = 0; i < question.size(); i++) {
+        std::cout << question[i].length() << std::endl;
+    }
+    
+
     std::locale::global(std::locale::classic()); //clears the special characters interpreter
     return 0;
 }
