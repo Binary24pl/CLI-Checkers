ba2cedd05cc7322ec90efd1eaea8cfb09d2a5fdc
Binary24pl <scpbinary@gmail.com>
2025-07-27 19:42:45 +0200

    yet another revision of signal library, now I have reminded myself of func pointers

--- objects/sgl_values.h
+++ objects/sgl_values.h
@@ -1,17 +1,4 @@
 #ifndef SGL
 #define SGL
 
-enum sgl_type_idx {
-    SGL_TYPE_INT,
-    SGL_TYPE_CHAR,
-    SGL_TYPE_STRING,
-    SGL_TYPE_TOTAL
-};
-
-struct sgl_wrong_data_type {};
-
-//remember always &typeid(typename) for our la values
-void sgl_make_reco_list(std::vector<const std::type_info*>& our_list);
-void sgl_debbug_read(std::vector<const std::type_info*>& our_list);
-
 #endif
