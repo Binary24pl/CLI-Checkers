41c5fda653f525d27b857ba28c2a7fe53d720817
Binary24pl <scpbinary@gmail.com>
2025-08-08 10:33:27 +0200

    redesigning the signal library

--- objects/sgl_values.h
+++ objects/sgl_values.h
@@ -1,24 +1,6 @@
 #ifndef SGL
 #define SGL
 
-#define SGL_DATA_NEW true
-
-struct sgl_data {
-    void* value;
-    void (*caster_func)(void*&, sgl_data&);
-    void (*recaster_func)(void*&, sgl_data&);
-    void (*cleanup_func)(sgl_data&);
-    const std::type_info* identifier;
-};
-//for memory heap safety, always set value as nullptr at the begining of its life
-
-template<typename cast> void sgl_data_caster(void*& val, sgl_data& holder);
-template<typename cast> void sgl_data_recaster(void*& val, sgl_data& holder);
-
-template<typename cast> void sgl_data_setter(sgl_data& holder);
-template<typename cast> void sgl_data_cleaner(sgl_data& holder);
-
-template<typename pass> void sgl_data_passer(const pass& val, sgl_data& holder, const bool& is_new = false);
-template<typename pass> void sgl_data_repasser(pass& val, sgl_data& holder);
+//soon
 
 #endif
