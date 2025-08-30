0781b97d9f92fcf3017901a72ae4b76eb5b50c2b
Binary24pl <scpbinary@gmail.com>
2025-08-30 17:19:50 +0200

    about to make colide check

--- objects/gam_board.h
+++ objects/gam_board.h
@@ -11,7 +11,7 @@ void gam_board::init()
 
     for(int on_hght = 0; on_hght < this->board_height; on_hght++) {
         for(int on_wdth = 0; on_wdth < this->board_width; on_wdth++) {
-            this->board_pos[on_hght][on_hght] = GAM_REP_UNPLAYABLE;
+            this->board_pos[on_hght][on_wdth] = GAM_REP_UNPLAYABLE;
 
             if(on_hght % 2 == on_wdth % 2) {
                 if(on_hght < 2) {
@@ -71,45 +71,38 @@ bool gam_board::find_me(gam_pawn_rep& compared)
 
 bool gam_board::is_movable(const bool& whose_turn, const int& directions, const common_position& current_pos)
 {
-    if(current_pos.on_height < 0 || current_pos.on_width < 0 || current_pos.on_height >= this->board_height || current_pos.on_width >= this->board_width) return false;
+    if(current_pos.on_height < 0 || current_pos.on_height >= this->board_height || current_pos.on_width < 0 | current_pos.on_width >= this->board_width) return false;
 
-    bool unlimited = false;
-
-    if(directions == 0) unlimited = true;
-
-    bool (gam_board::*direct_check_up_left)(const common_position& current_pos) = nullptr;
-    bool (gam_board::*direct_check_up_right)(const common_position& current_pos) = nullptr;
-    bool (gam_board::*direct_check_down_left)(const common_position& current_pos) = nullptr;
-    bool (gam_board::*direct_check_down_right)(const common_position& current_pos) = nullptr;
+    bool (gam_board::*up_left)(const common_position&) = nullptr;
+    bool (gam_board::*up_right)(const common_position&) = nullptr;
+    bool (gam_board::*down_left)(const common_position&) = nullptr;
+    bool (gam_board::*down_right)(const common_position&) = nullptr;
 
     if(whose_turn) {
-        direct_check_up_left = &gam_board::side_way_check<-1, -1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
-        
-        direct_check_up_right = &gam_board::side_way_check<-1, 1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
-
-        direct_check_up_left = &gam_board::side_way_check<1, -1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
-        
-        direct_check_up_right = &gam_board::side_way_check<1, 1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+        up_left = &gam_board::side_way_check<-1, -1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+        up_right = &gam_board::side_way_check<-1, 1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+        down_left = &gam_board::side_way_check<1, -1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+        down_right = &gam_board::side_way_check<1, 1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
     } else {
-        direct_check_up_left = &gam_board::side_way_check<-1, -1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
-        
-        direct_check_up_right = &gam_board::side_way_check<-1, 1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
-
-        direct_check_up_left = &gam_board::side_way_check<1, -1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
-        
-        direct_check_up_right = &gam_board::side_way_check<1, 1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+        up_left = &gam_board::side_way_check<-1, -1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+        up_right = &gam_board::side_way_check<-1, 1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+        down_left = &gam_board::side_way_check<1, -1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+        down_right = &gam_board::side_way_check<1, 1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
     }
 
-    if(unlimited) {
-        return (this->*direct_check_up_left)(current_pos) || (this->*direct_check_up_right)(current_pos) || (this->*direct_check_down_left)(current_pos) || (this->*direct_check_down_right)(current_pos);
-    } else {
-        if(directions > 0) {
-            return (this->*direct_check_down_left)(current_pos) || (this->*direct_check_down_right)(current_pos);
-        } else {
-            return (this->*direct_check_up_left)(current_pos) || (this->*direct_check_up_right)(current_pos);
-        }
-    }
+    if(directions > 0) {
+        bool results = (this->*down_left)(current_pos) || (this->*down_right)(current_pos);
 
+        return results;
+    } else if(directions < 0) {
+        bool results = (this->*up_left)(current_pos) || (this->*up_right)(current_pos);
+
+        return results;
+    } else {
+        bool results = (this->*down_left)(current_pos) || (this->*down_right)(current_pos) || (this->*up_left)(current_pos) || (this->*up_right)(current_pos);
+
+        return results;
+    }
 
     return false;
 }
@@ -117,28 +110,11 @@ bool gam_board::is_movable(const bool& whose_turn, const int& directions, const 
 template<int fronts, int sides, gam_pawn_rep strikeable_pawn, gam_pawn_rep strikable_jokey>
 bool gam_board::side_way_check(const common_position& current_pos)
 {
-    int possible_depth = 0;
+    int possible_steps = 0;
 
-    common_position step;
-    step.on_height = current_pos.on_height + fronts;
-    step.on_width = current_pos.on_width + sides;
+    std::cout << current_pos.on_height << " : " << current_pos.on_width << " -conf(" << fronts << ", " << sides << "); ";
 
-    if(step.on_height >= 0 && step.on_height < this->board_height && step.on_width >= 0 && step.on_width < this->board_width) {
-        possible_depth += 1;
-
-        step.on_height += fronts;
-        step.on_width += sides;
-
-        if(step.on_height >= 0 && step.on_height < this->board_height && step.on_width >= 0 && step.on_width < this->board_width) {
-            possible_depth += 1;
-        }
-    }
-
-    if(possible_depth != 0) {
-        if(this->board_pos[current_pos.on_height + fronts][current_pos.on_width + sides] == GAM_REP_EMPTY) return true;
-
-        if(possible_depth == 2 && (this->board_pos[current_pos.on_height + fronts][current_pos.on_width + sides] == strikeable_pawn || this->board_pos[current_pos.on_height + fronts][current_pos.on_width + sides] == strikable_jokey) && this->board_pos[current_pos.on_height + (2 * fronts)][current_pos.on_width + (2 * sides)] == GAM_REP_EMPTY) return true;
-    }
+    std::cout << possible_steps << std::endl;
 
     return false;
 }
