9182bbbbd01aec2a9dd2c2b83d221764297a2757
Binary24pl <scpbinary@gmail.com>
2025-09-01 10:12:21 +0200

    did the re-orgo

---
+++ objects/gam_board_mov.h
@@ -0,0 +1,164 @@
+//movable lounge
+
+void gam_board::give_movable(const bool& whose_turn, std::vector<common_position>& positions)
+{
+    if(positions.size() != 0) positions.clear();
+
+    bool (gam_board::*ourteam_check)(const bool&, const common_position&) = nullptr;
+
+    if(whose_turn) {
+        ourteam_check = &gam_board::check_movable<GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+    } else {
+        ourteam_check = &gam_board::check_movable<GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+    }
+
+    for(int on_hght = 0; on_hght < this->board_height; on_hght++) {
+        for(int on_wdth = 0; on_wdth < this->board_width; on_wdth++) {
+            common_position temp;
+            
+            temp.on_height = on_hght;
+            temp.on_width = on_wdth;
+
+            if((this->*ourteam_check)(whose_turn, temp)) {
+                //std::cout << temp.on_height << " : " << temp.on_width << " - is moveable" << std::endl;
+
+                positions.push_back(temp);
+            }
+        }
+    }
+}
+
+template<gam_pawn_rep pawn, gam_pawn_rep jokey>
+bool gam_board::check_movable(const bool& whose_turn, const common_position& our_pos)
+{
+    if(our_pos.on_height < 0 || our_pos.on_height >= this->board_height || our_pos.on_width < 0 || our_pos.on_width >= this->board_width) return false;
+
+    gam_pawn_rep our_pawn = this->board_pos[our_pos.on_height][our_pos.on_width];
+
+    bool (gam_board::*up_left)(const common_position) = nullptr;
+    bool (gam_board::*up_right)(const common_position) = nullptr;
+    bool (gam_board::*down_left)(const common_position) = nullptr;
+    bool (gam_board::*down_right)(const common_position) = nullptr;
+
+    if(whose_turn) {
+        up_left = &gam_board::check_sideways<-1, -1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+        up_right = &gam_board::check_sideways<-1, 1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+        down_left = &gam_board::check_sideways<1, -1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+        down_right = &gam_board::check_sideways<1, 1, GAM_REP_DARK_PAWN, GAM_REP_DARK_JOKEY>;
+    } else {
+        up_left = &gam_board::check_sideways<-1, -1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+        up_right = &gam_board::check_sideways<-1, 1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+        down_left = &gam_board::check_sideways<1, -1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+        down_right = &gam_board::check_sideways<1, 1, GAM_REP_LIGHT_PAWN, GAM_REP_LIGHT_JOKEY>;
+    }
+
+    if(our_pawn == pawn || our_pawn == jokey) {
+        //std::cout << our_pos.on_height << " : " << our_pos.on_width << " - board piece" << std::endl;
+
+        if(our_pawn == pawn) {
+            if(whose_turn) {
+                return (this->*up_left)(our_pos) || (this->*up_right)(our_pos);
+            } else {
+                return (this->*down_left)(our_pos) || (this->*down_right)(our_pos);
+            }
+        } else {
+            return (this->*up_left)(our_pos) || (this->*up_right)(our_pos) || (this->*down_left)(our_pos) || (this->*down_right)(our_pos);
+        }
+    }
+
+
+    return false;
+}
+
+template<int fronts, int sides, gam_pawn_rep strikable_pawn, gam_pawn_rep stikable_jokey>
+bool gam_board::check_sideways(const common_position our_pos)
+{
+    //std::cout << "checking for fronts of: " << fronts << " and sides of : " << sides << ". ";
+
+    common_position step_one, step_two;
+
+    step_one.on_height = our_pos.on_height + fronts;
+    step_one.on_width = our_pos.on_width + sides;
+
+    step_two.on_height = our_pos.on_height + (fronts * 2);
+    step_two.on_width = our_pos.on_width + (sides * 2);
+
+    int possible_steps = 0;
+
+    if(step_one.on_height >= 0 && step_one.on_width >= 0 && step_one.on_height < this->board_height && step_one.on_width < this->board_width) {
+        possible_steps += 1;
+
+        if(step_two.on_height >= 0 && step_two.on_width >= 0 && step_two.on_height < this->board_height && step_two.on_width < this->board_width) {
+            possible_steps += 1;
+        }
+    }
+
+    //std::cout << "With " << possible_steps << " possible steps" << std::endl;
+
+    if(possible_steps == 0) return false;
+
+    if(this->board_pos[step_one.on_height][step_one.on_width] == GAM_REP_EMPTY) return true;
+
+    if(possible_steps == 1) return false;
+
+    if(this->board_pos[step_one.on_height][step_one.on_width] == strikable_pawn || this->board_pos[step_one.on_height][step_one.on_width] == stikable_jokey) {
+        if(this->board_pos[step_two.on_height][step_two.on_width] == GAM_REP_EMPTY) return true;
+    }
+
+    return false;
+}
+
+void gam_board::handle_position_slct(int on_hght, int on_wdth, common_board_interface& interface, const std::vector<common_position>& selectable)
+{
+    if(this->board_pos[on_hght][on_wdth] == GAM_REP_EMPTY) {
+        common_board_playable temp;
+
+        temp.position.on_height = on_hght;
+        temp.position.on_width = on_wdth;
+        temp.current_state = CMN_STAT_NEITHER;
+
+        interface.playable.push_back(temp);
+        return;
+    }
+
+    common_board_pawns temp;
+            
+    temp.position.on_height = on_hght;
+    temp.position.on_width = on_wdth;
+    temp.current_state = CMN_STAT_NEITHER;
+
+    switch(this->board_pos[on_hght][on_wdth]) {
+        case GAM_REP_LIGHT_PAWN : {
+            temp.type = CMN_PAWN_LIGHT;
+            break;
+        }
+
+        case GAM_REP_LIGHT_JOKEY : {
+            temp.type = CMN_JOKEY_LIGHT;
+            break;
+        }
+
+        case GAM_REP_DARK_PAWN : {
+            temp.type = CMN_PAWN_DARK;
+            break;
+        }
+
+        case GAM_REP_DARK_JOKEY : {
+            temp.type = CMN_JOKEY_DARK;
+            break;
+        }
+
+        default : {
+            break;
+        }
+    }
+
+    for(int i = 0; i < selectable.size(); i++) {
+        if(on_hght == selectable[i].on_height && on_wdth == selectable[i].on_width) {
+            temp.current_state = CMN_STAT_SELECTABLE;
+            break;
+        }
+    }
+
+    interface.pawns.push_back(temp);
+}
---
+++ objects/gam_board_slt.h
@@ -0,0 +1,91 @@
+//options lounge
+
+void gam_board::give_options(const bool& whose_turn, gam_piece_move& our_opts, const common_position& our_pos, const bool& unlimited)
+{
+    void (gam_board::*up_left)(gam_piece_move&, const common_position&, const bool&) = nullptr;
+    void (gam_board::*up_right)(gam_piece_move&, const common_position&, const bool&) = nullptr;
+    void (gam_board::*down_left)(gam_piece_move&, const common_position&, const bool&) = nullptr;
+    void (gam_board::*down_right)(gam_piece_move&, const common_position&, const bool&) = nullptr;
+}
+
+template<int fronts, int sides, gam_pawn_rep strikable_pawn, gam_pawn_rep strikable_jokey>
+void gam_board::sideway_options(gam_piece_move& our_opts, const common_position& our_pos, const bool& unlimited)
+{
+    common_position next_pos;
+    next_pos.on_height = our_pos.on_height + fronts;
+    next_pos.on_width = our_pos.on_width + sides;
+
+    if(next_pos.on_height < 0 || next_pos.on_width < 0 || next_pos.on_height >= this->board_height || next_pos.on_width >= this->board_width) return;
+
+    common_position no_strike;
+    no_strike.on_height = -1;
+    no_strike.on_width = -1;
+
+    if(this->board_pos[next_pos.on_height][next_pos.on_width] != GAM_REP_EMPTY && this->board_pos[next_pos.on_height][next_pos.on_width] != strikable_pawn && this->board_pos[next_pos.on_height][next_pos.on_width] != strikable_jokey) return;
+
+    if(this->board_pos[next_pos.on_height][next_pos.on_width] == GAM_REP_EMPTY) {
+        our_opts.possible_next_strike.push_back(false);
+        our_opts.possible_points.push_back(next_pos);
+        our_opts.strike_positions.push_back(no_strike);
+
+        if(unlimited) {
+            this->sideway_options<fronts, sides, strikable_pawn, strikable_jokey>(our_opts, next_pos, unlimited);
+        }
+
+        return;
+    }
+
+    common_position behind_enemy;
+    behind_enemy.on_height = next_pos.on_height + fronts;
+    behind_enemy.on_width = next_pos.on_width + sides;
+
+    if(behind_enemy.on_height < 0 || behind_enemy.on_width < 0; behind_enemy.on_height >= this->board_height || behind_enemy.on_width >= board_width) return;
+
+    if(this->board_pos[behind_enemy.on_height][behind_enemy.on_width] != GAM_REP_EMPTY) return;
+
+    our_opts.possible_points.push_back(behind_enemy);
+    our_opts.strike_positions.push_back(next_pos);
+
+    this->strike_options<fronts, sides, strikable_pawn, strikable_jokey>(our_opts, behind_enemy);
+}
+
+template<int fronts, int sides, gam_pawn_rep strikable_pawn, gam_pawn_rep strikable_jokey>
+void gam_board::strike_options(gam_piece_move& our_opts, const common_position& our_pos)
+{
+    const int coords = 3;
+
+    common_position calc_vals[coords];
+
+    calc_vals[0].on_height = fronts * -1;
+    calc_vals[0].on_width = sides * -1;
+
+    calc_vals[1].on_height = fronts;
+    calc_vals[1].on_width = sides * -1;
+
+    calc_vals[2].on_height = fronts * -1;
+    calc_vals[2].on_width = sides;
+
+    common_position possible_enemies[coords];
+    common_position possibly_behind_em[coords];
+
+    bool multiple = false;
+
+    for(int i = 0; i < coords; i++) {
+        possible_enemies[i].on_height = our_pos.on_height + (calc_vals[i].on_height * 1);
+        possible_enemies[i].on_width = our_pos.on_width + (calc_vals[i].on_width * 1);
+
+        possibly_behind_em[i].on_height = our_pos.on_height + (calc_vals[i].on_height * 1);
+        possibly_behind_em[i].on_width = our_pos.on_width + (calc_vals[i].on_width * 1);
+    }
+
+    for(int check = 0; check < coords; check++) {
+        if(possibly_behind_em[check].on_height >= 0 && possibly_behind_em[check].on_width >= 0 && possibly_behind_em[check].on_height < this->board_height && possibly_behind_em[check].on_width < this->board_width) {
+            if((this->board_pos[possible_enemies[check].on_height][possible_enemies[check].on_width] == strikable_pawn || this->board_pos[possible_enemies[check].on_height][possible_enemies[check].on_width] == strikable_jokey) && this->board_pos[possibly_behind_em[check].on_height][possibly_behind_em[check].on_width] == GAM_REP_EMPTY) {
+                multiple = true;
+                break;
+            }
+        }
+    }
+
+    our_opts.possible_next_strike.push_back(multiple);
+}
